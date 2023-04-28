7f7569d63c539e9be81099e279a5eb544a9fe660
ly <198six@naver.com>
2023-04-28 22:08:44 +0900

    초기 커밋
    커밋

---
+++ Day02/src/HelloWorld02.java
@@ -0,0 +1,6 @@
+
+public class HelloWorld02 {
+	public static void main(String args[]) {
+		System.out.println("오늘은 2일차야.");
+	}
+}
---
+++ Day02/src/javaStructure.java
@@ -0,0 +1,13 @@
+// 1. 자바는 클래스이름과 파일명이 같아야합니다.
+// 2. 자바는 대소문자를 구분합니다.
+// ex) Java java -> 같지 않아요
+// 3. { } 중괄호로 문단, 문장을 구분합니다.
+//4. 자바의 문장의 끝은 ;을 사용합니다.
+public class javaStructure {
+
+	public static void main(String arg[]) {
+//		System.out.println("하악하악");
+		// System.out.println("하악하악") <-- 오류
+		// 문장의 끝에는; 이 필요해서 오류.
+	}
+}
